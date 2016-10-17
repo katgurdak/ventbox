811b6eb19c06e0bbb4958417bebc1b539f01fbe0
katgurdak <katgurdak@gmail.com>
2016-10-17 16:58:45 -0700

    Added functionality to button  - On submit, will redirect path to 'quotes'  - 'quotes' will show a quote (hardcoded for now)

--- client/app/vent/vent.js
+++ client/app/vent/vent.js
@@ -1,5 +1,7 @@
 angular.module('ventbox.vent', [])
 
-.controller('VentController', function($scope) {
-
-})
+.controller('VentController', function($scope, $location) {
+  $scope.getQuote = function() {
+    $location.path('quotes');
+  }
+});

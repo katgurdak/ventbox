454c8fd8cc38aa55ebab0887e64e9069e0e85d35
katgurdak <katgurdak@gmail.com>
2016-10-17 19:13:55 -0700

    Random quote will now appear on '/quotes'
     - Added button to give user the option to vent more (uses QuotesController ventMore method)
     - '/quotes' now shows a random quote
     - Factory stores quotes

--- client/app/quotes/quotes.js
+++ client/app/quotes/quotes.js
@@ -1,5 +1,19 @@
 angular.module('ventbox.quotes', [])
 
-.controller('QuotesController', function($scope) {
-
+.controller('QuotesController', function($scope, $location, Quotes) {
+	$scope.randomQuote = Quotes.getRandomQuote();
+  $scope.ventMore = function() {
+  	$location.path('vent');
+  };
 })
+
+.factory('Quotes', function() {
+  var quotes = ['“How wonderful it is that nobody need wait a single moment before starting to improve the world.” ~Anne Frank', '“Start where you are. Use what you have. Do what you can.” ~Arthur Ashe'];
+  var getRandomQuote = function() {
+  	return quotes[Math.floor(Math.random() * quotes.length)];
+  };
+
+  return {
+  	getRandomQuote: getRandomQuote
+  }
+});
--- client/app/vent/vent.js
+++ client/app/vent/vent.js
@@ -3,5 +3,5 @@ angular.module('ventbox.vent', [])
 .controller('VentController', function($scope, $location) {
   $scope.getQuote = function() {
     $location.path('quotes');
-  }
+  };
 });
